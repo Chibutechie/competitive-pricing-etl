0598ee5e973f242d28fb38078df32e91836778d7
Chibueze <Chibuezeanalyst@gmail.com>
2026-02-17 07:49:01 -0600

    transformed data from pq to csv

---
+++ transform.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+url= "hf://datasets/electricsheepafrica/nigerian_retail_and_ecommerce_competitor_pricing_datasets/data/nigerian_retail_and_ecommerce_competitor_pricing_datasets.parquet"
+
+df =pd.read_parquet(url)
+
+#convert file from pq to csv
+
+df.to_csv("nigerian_retail_and_ecommerce_competitor_pricing_datasets.csv", index=False)
+
+print(df.head())
