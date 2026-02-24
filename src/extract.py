ee0cdb2f5e38d73e0476b04dc21c0cda4fecc6c3
Chibueze <Chibuezeanalyst@gmail.com>
2026-02-23 19:56:49 -0600

    extract csv and move to data

--- src/extract.py
+++ src/extract.py
@@ -1,7 +1,15 @@
+from pathlib import Path
 import pandas as pd
 
-url="hf://datasets/electricsheepafrica/nigerian_retail_and_ecommerce_competitor_pricing_datasets/data/nigerian_retail_and_ecommerce_competitor_pricing_datasets.parquet"
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-df = pd.read_parquet(url)
+raw_path = BASE_DIR / "data" / "raw"
+raw_path.mkdir(parents=True, exist_ok=True)
+
+df = pd.read_parquet(
+    "hf://datasets/electricsheepafrica/nigerian_retail_and_ecommerce_competitor_pricing_datasets/data/nigerian_retail_and_ecommerce_competitor_pricing_datasets.parquet"
+)
+
+df.to_csv(raw_path / "retail_and_ecommerce_competitor_pricing.csv", index=False)
 
 print(df.head())
